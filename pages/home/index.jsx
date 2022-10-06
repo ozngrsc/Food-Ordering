f55ad53dab6bdff16bb8d729d168565ecbd3feb0
ozngrsc <ozangursucu@hotmail.com>
2022-10-06 15:16:02 +0300

    first commit

---
+++ pages/home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div className="text-red-600 font-dancing text-center text-3xl">
+      Home Page
+    </div>
+  );
+};
+
+export default Index;
