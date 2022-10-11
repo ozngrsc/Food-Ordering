e69d2da5aebed24b79e549e849e62e1d1fd47db0
ozngrsc <ozangursucu@hotmail.com>
2022-10-11 15:31:21 +0300

    add menuWrapper and menuItem

---
+++ components/products/MenuItem.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+
+const MenuItem = () => {
+  return (
+    <div className="bg-secondary rounded-3xl">
+      <div className="w-full bg-[#f1f2f3] h-[210px] grid place-content-center rounded-bl-[46px] rounded-tl-2xl rounded-tr-2xl">
+        <div className="relative w-36 h-36 hover:scale-110 transition-all">
+          <Image src="/images/f1.png" alt="" layout="fill" />
+        </div>
+      </div>
+      <div className="text-white p-[25px]">
+        <h4 className="text-xl font-semibold mb-2">Delicious Pizza</h4>
+        <p className="text-[15px]">
+          Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit,
+          magnam voluptatem repellendus sed eaque
+        </p>
+        <div className="flex justify-between items-center mt-4">
+          <span>$20</span>
+          <button className="btn-primary w-10 h-10 rounded-full !p-0 grid place-content-center">
+            <FaShoppingCart />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Campaigns from "../../components/Campaigns";
 import Carousel from "../../components/Carousel";
+import MenuWrapper from "../../components/products/MenuWrapper";
 
 const Index = () => {
   return (
     <div>
       <Carousel />
       <Campaigns />
+      <MenuWrapper />
     </div>
   );
 };
