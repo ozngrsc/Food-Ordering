5a60fb895ac4ec2832b36e2a461d17b0d9609346
ozngrsc <ozangursucu@hotmail.com>
2022-10-14 11:44:17 +0300

    add nextjs layout

--- components/customers/Customers.jsx
+++ components/customers/Customers.jsx
@@ -50,7 +50,7 @@ const Customers = () => {
   };
 
   return (
-    <div className="container mx-auto my-20">
+    <div className="container mx-auto mb-20 mt-12">
       <Title className="text-[40px] text-center ">
         What Says Our Customers
       </Title>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Footer from "../components/layout/Footer";
-import Header from "../components/layout/Header";
 import Home from "./home";
 
 export default function Index() {
@@ -17,9 +15,7 @@ export default function Index() {
           crossorigin="true"
         />
       </Head>
-      <Header />
       <Home />
-      <Footer />
     </div>
   );
 }
