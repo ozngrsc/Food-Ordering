16a7e57d6bab2cf44d59cccf5c7f96884f8f7279
ozngrsc <ozangursucu@hotmail.com>
2022-10-20 10:50:31 +0300

    add profile page orders component

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import Account from "../../components/profile/Account";
+import Order from "../../components/profile/Order";
 import Password from "../../components/profile/Password";
 
 const Profile = () => {
@@ -60,6 +61,7 @@ const Profile = () => {
       </div>
       {tabs === 0 && <Account />}
       {tabs === 1 && <Password />}
+      {tabs === 2 && <Order />}
     </div>
   );
 };
