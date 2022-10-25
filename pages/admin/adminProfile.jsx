60bef9947d9259b08365c89ed0f31e9840e24ea9
ozngrsc <ozangursucu@hotmail.com>
2022-10-25 13:10:56 +0300

    add admin page footer component

--- pages/admin/adminProfile.jsx
+++ pages/admin/adminProfile.jsx
@@ -5,6 +5,7 @@ import Account from "../../components/profile/Account";
 import Order from "../../components/admin/Order";
 import Password from "../../components/profile/Password";
 import Category from "../../components/admin/Category";
+import Footer from "../../components/admin/Footer";
 
 const Profile = () => {
   const [tabs, setTabs] = useState(0);
@@ -73,6 +74,7 @@ const Profile = () => {
       {tabs === 0 && <Product />}
       {tabs === 1 && <Order />}
       {tabs === 2 && <Category />}
+      {tabs === 3 && <Footer />}
     </div>
   );
 };
